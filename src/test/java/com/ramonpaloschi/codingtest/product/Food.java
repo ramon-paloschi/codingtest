e24227cb550b185b28bc5070169bb66ff7cfd6dd
ramon-paloschi <69748169+ramon-paloschi@users.noreply.github.com>
2020-08-16 10:58:32 +0200

    Create Food.java

---
+++ src/test/java/com/ramonpaloschi/codingtest/product/Food.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ramonpaloschi.codingtest.product;
+
+/**
+ *
+ * @author ramon
+ */
+public class Food extends Product {
+    private String category = "";
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
